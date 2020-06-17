5599717ed057aba67993912fb0df48291ad06622
erriveros <erriveros@miuandes.cl>
2020-06-17 15:55:44 -0400

    categories market done

---
+++ components/constants/mocks.js
@@ -0,0 +1,81 @@
+const categories = [
+    {
+        id:'organizacion',
+        name: 'Organization',
+        tags: ['products', 'pesitas'],
+        count: 147,
+        image: ''
+
+    },
+    {
+        id:'strenght',
+        name: 'Strenght',
+        tags: ['products', 'pesitas'],
+        count: 147,
+        image: ''
+
+    },
+    {
+        id:'conditioning',
+        name: 'Conditioning',
+        tags: ['products', 'pesitas'],
+        count: 147,
+        image: ''
+
+    },
+    {
+        id:'core',
+        name: 'Core',
+        tags: ['products', 'pesitas'],
+        count: 147,
+        image: ''
+
+    },
+    {
+        id:'mobility',
+        name: 'Mobility',
+        tags: ['products', 'pesitas'],
+        count: 147,
+        image: ''
+
+    },
+    {
+        id:'featured',
+        name: 'Featured',
+        tags: ['products', 'pesitas'],
+        count: 147,
+        image: ''
+
+    },
+];
+
+const products = [
+    {
+        id: 1,
+        name: 'Home gym weight set',
+        description: 'Complete with everything you need for an effective weight training session, the ETHOS® 205 lb. Olympic Rubber Bumper Plate Set features 6 bumper plates with a resilient rubber construction that offers a quieter drop with minimal bounce.',
+        tags: ['Strenght', 'pesas'],
+        gallery: [
+            'https://images-na.ssl-images-amazon.com/images/I/61kwkttOf5L._AC_SX466_.jpg',
+            'https://allzone.es/331737-large_default/pesas-y-mancuernas-juego-de-pesas-2-de-mancuernas-605-kg-vxl90375.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhx2xVXHoH9Ca_7D9AqSTyX-938T562j1nfLgIGYSEbiPZdOmm&usqp=CAU',
+            'https://images-na.ssl-images-amazon.com/images/I/61rC6cVQfDL._AC_SX466_.jpg',
+            'https://okdiario.com/img/2020/03/30/empezar-a-levantar-pesas-655x368.jpeg'
+
+
+
+        ]
+    }
+
+];
+
+const profile =[
+
+];
+
+
+export {
+    categories,
+    products,
+    profile
+}
--- components/presentation/index.js
+++ components/presentation/index.js
@@ -1,5 +1,15 @@
 import Post from './post';
+import Block from './Block'
+import Text from './Text'
+import Badge from './Badge'
+import Card from './Card'
+import Button from './Button'
 
 export {
-    Post
+    Post,
+    Block,
+    Badge,
+    Button,
+    Card,
+    Text
 };
